f2bf619a79bc2baee64f38eab0cfa34113eae9f3
Drasoch <20261ctb0100031@estudantes.ifpr.edu.br>
2026-04-10 02:13:02 +0000

    Aula 11 - 1052: mes

---
+++ beecrowd/1052-mes.py
@@ -0,0 +1,38 @@
+'''
+Problema: beecrowd | NúmeroDoProblema
+Data: ano.mes.dia
+Estudante: Pedro André P. A. Blaka
+'''
+# Objetivo: descrição sucinta do que o programa deve fazer
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: quais dados o programa recebe e de que tipo?
+# Processamento: ler, e verificar se o numero possui um mes correspondente ou nao
+# Saída: deve ser mostrado o mes a qual o numero corresponde
+
+M = int(input())     # cada numero corresponde a um mes 
+
+if M == 1:
+    print("January")
+elif M == 2:
+    print("February")
+elif M == 3:
+    print("March")
+elif M == 4:
+    print("April")
+elif M == 5:
+    print("May")
+elif M == 6:
+    print("June")
+elif M == 7:
+    print("July") 
+elif M == 8:
+    print("August")
+elif M == 9:
+    print("September")
+elif M == 10:
+    print("October")
+elif M == 11:
+    print("November")
+elif M == 12:
+    print("December")
