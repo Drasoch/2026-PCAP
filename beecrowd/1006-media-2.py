8a38c7034787741106c96842d05f341c91c9cd64
Drasoch <20261ctb0100031@estudantes.ifpr.edu.br>
2026-04-10 00:13:43 +0000

    Aula 10 - 1000: Hello world

---
+++ beecrowd/1006-media-2.py
@@ -0,0 +1,22 @@
+'''
+Problema: beecrowd | 1006
+Data: 2026.04.09
+Estudante: Pedro André P. A. Blaka
+'''
+# Objetivo: ler tres valores de notas de umaluno e calcular a media ponderada delas
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: 
+# Processamento: calcular a media 
+# Saída:
+
+
+A = float(input())
+B = float(input())
+C = float(input())
+
+
+media = (A * 2 + B * 3 + C * 5) / 10
+
+print(f"MEDIA = {media}")
+
